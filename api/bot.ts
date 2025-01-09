53402f7ab57dc51bd2c4d0888a0cc5b73f3470f1
rexcare <clever.fox.w@gmail.com>
2025-01-09 13:11:18 -0500

    feat/add test

--- api/bot.ts
+++ api/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, webhookCallback, Context, session, SessionFlavor, InlineKeyboard } from "grammy";
+import { Bot, webhookCallback, Context, session, SessionFlavor } from "grammy";
 import * as dotenv from "dotenv";
 import { createTask, getProjectById, getProjects, getSections } from "./util/asana";
 import { getSetting, updateSettings,  } from "./util/supabase";
@@ -19,42 +19,32 @@ interface SessionData {
   // Flavor the context type to include sessions.
 type MyContext = Context & SessionFlavor<SessionData>;
 
-let botcontext:any; 
+// let botcontext:any; 
 const bot = new Bot<MyContext>(token);
 
-// Install session middleware, and define the initial session value.
-async function initial(): Promise<SessionData> {
-    return { workspace: '', project: ''};
-}
+// // Install session middleware, and define the initial session value.
+// async function initial(): Promise<SessionData> {
+//     return { workspace: '', project: ''};
+// }
   
-bot.use(session({ initial: initial as any }));
+// bot.use(session({ initial: initial as any }));
 
-const nameArray = [
-    {
-        gid: "1209093882208970",
-        name: "Brian",
-    },
-    {
-        gid: "291735074243751",
-        name: "Mike",
-    },
-    {
-        gid: "1180519359737765",
-        name: "Mark",
-    },
-];
+// const nameArray = [
+//     {
+//         gid: "1209093882208970",
+//         name: "Brian",
+//     },
+//     {
+//         gid: "291735074243751",
+//         name: "Mike",
+//     },
+//     {
+//         gid: "1180519359737765",
+//         name: "Mark",
+//     },
+// ];
 
 bot.command("start", async (ctx: any) => {
-    const inlineKeyboard = new InlineKeyboard().text("click", "click-payload");
-    await ctx.reply("Curious? Click me!", { reply_markup: inlineKeyboard });
-});
-
-bot.command("config", async (ctx: any) => {
-    const setting = await getSetting();
-    const {name} = await getProjectById(setting.project);
-    const replyText =
-        `You can check your setting.\n`
-    
     const replyMarkup = {
         inline_keyboard: [
             [
@@ -66,161 +56,177 @@ bot.command("config", async (ctx: any) => {
         ],
     };
     
-    await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
+    await ctx.reply('replyText', { reply_markup: replyMarkup, parse_mode: "Markdown" });
 });
 
-// bot.callbackQuery("click-payload", async (ctx) => {
-//     await ctx.reply("click-payload");
-// })
-
-// bot.callbackQuery("changeProject", async (ctx) => {
-//     const projects = await getProjects();
-
+// bot.command("config", async (ctx: any) => {
+//     const setting = await getSetting();
+//     const {name} = await getProjectById(setting.project);
 //     const replyText =
-//         `click one project to create task on.\n`
+//         `You can check your setting.\n`
     
 //     const replyMarkup = {
-//         inline_keyboard: projects.map((project:any) => [
-//             { text: project.name, callback_data: `changeProject:${project.gid}` }
-//         ]),
+//         inline_keyboard: [
+//             [
+//                 { text: "--- Project ---", callback_data: "create_task_title" }, 
+//             ], 
+//             [
+//                 { text: `✎ ${name}`, callback_data: "changeProject" }
+//             ]
+//         ],
 //     };
-
+    
 //     await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
 // });
 
+bot.callbackQuery("changeProject", async (ctx) => {
+    await ctx.reply('replyText');
+    // const projects = await getProjects();
 
-// Handle other messages.
-bot.on("message", async (ctx: any) => {
-    const messageText = ctx.message.text || "";
-    console.log("message ==> ", messageText);
+    // const replyText =
+    //     `click one project to create task on.\n`
+    
+    // const replyMarkup = {
+    //     inline_keyboard: projects.map((project:any) => [
+    //         { text: project.name, callback_data: `changeProject:${project.gid}` }
+    //     ]),
+    // };
 
-    if (messageText.toLowerCase().startsWith("createtask")) {
-        const parts = messageText.split(" ");
-
-        if (parts.length > 1) {
-            const loading = await ctx.reply("processing...");
-            const secondWord = parts[1].trim();
-            const user = nameArray.find((user) => user.name.toLowerCase() === secondWord.toLowerCase());
-
-            if (!user) {
-                const replyText = `📢 Can't find specific name - ${secondWord}`;
-                await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
-                return  ctx.reply(replyText, { parse_mode: "Markdown" });
-            }
-
-            const setting = await getSetting();
-
-            const thirdWord = parts[2].trim();
-
-            const sections = await getSections(setting.project);
-
-            // let section = {};
-            // let i = 0;
-            // while(1)
-            // {
-            //     let _sectionName = parts.slice(2, 3 + i).join(" ");
-            //     const _section = sections.map((section:any) => section.name.toLowerCase().startsWith(thirdWord.toLowerCase()));
-            //     if(_section.length === 1)
-            //         {
-            //             section = {..._section}
-            //             break;
-            //         }
-            //     i++;
-            // }
-
-            const section = sections.find((section:any) => section.name.toLowerCase().startsWith(thirdWord.toLowerCase()));
-
-            if (!section) {
-                console.log(`Can't find secction from ${messageText}`);
-                const replyText = `📢 Can't find secction from ${messageText}`;
-                await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
-                return  ctx.reply(replyText, { parse_mode: "Markdown" });
-            }
-
-            // Join the remaining parts of the message
-            const command = parts.slice(2+section.name.split(' ').length).join(" ");
-
-            let body = {
-                data: {
-                    name: command,
-                    assignee: user.gid,
-                    resource_subtype: "default_task",
-                    approval_status: "pending",
-                    assignee_status: "upcoming",
-                    assignee_section: section.gid,
-                    projects: [setting.project],
-                },
-            };
-
-            const task = await createTask(body);
-
-
-            const taskName = task.name;
-            const projectName = task.projects[0].name;
-            const sectionName = section.name;
-            const workspaceName = task.workspace.name;
-            const taskUrl = task.permalink_url;
-            const replyText =
-                `📢 Successfully created a task\n\n` +
-                `**${taskName}**\n\n` +
-                `🏠 Workspace: **${workspaceName}**\n` +
-                `🧰 Project: **${projectName}**\n` +
-                `🔖 Section: **${sectionName}**\n\n` +
-                `${taskUrl}`;
-            const replyMarkup = {
-                inline_keyboard: [[{ text: "View Task", url: taskUrl }]],
-            };
-            await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
-            await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
-            
-        }
-    }
+    // await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
 });
 
-bot.on("callback_query:data", async (ctx) => {
-    const callbackQuery = ctx.callbackQuery.data;
-    console.log('===callbackQuery', callbackQuery);
-    await ctx.reply('=====replyText')
-    if(callbackQuery.startsWith("changeProject"))
-    {    
-        const project = await getProjectById(callbackQuery.split(':')[1]);
-        await updateSettings(project.workspace.gid, project.gid)
+// bot.on("callback_query:data", async (ctx) => {
+//     const callbackQuery = ctx.callbackQuery.data;
+//     if(callbackQuery.startsWith("changeProject"))
+//     {    
+//         const project = await getProjectById(callbackQuery.split(':')[1]);
+//         await updateSettings(project.workspace.gid, project.gid)
 
-        const setting = await getSetting();
-        const {name} = await getProjectById(setting.project);
-        const replyText =
-            `You can check your setting.\n`
+//         const setting = await getSetting();
+//         const {name} = await getProjectById(setting.project);
+//         const replyText =
+//             `You can check your setting.\n`
         
-        const replyMarkup = {
-            inline_keyboard: [
-                [
-                    { text: "--- Project ---", callback_data: "create_task_title" }, 
-                ], 
-                [
-                    { text: `✎ ${name}`, callback_data: "changeProject" }
-                ]
-            ],
-        };
+//         const replyMarkup = {
+//             inline_keyboard: [
+//                 [
+//                     { text: "--- Project ---", callback_data: "create_task_title" }, 
+//                 ], 
+//                 [
+//                     { text: `✎ ${name}`, callback_data: "changeProject" }
+//                 ]
+//             ],
+//         };
         
-        await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
+//         await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
 
-    }
-    else {
-        console.log('callbackQuery', callbackQuery)
-    }
-    await ctx.answerCallbackQuery({
-        text: "You were curious, indeed!",
-      });
-})
+//     }
+//     else {
+//         console.log('callbackQuery', callbackQuery)
+//     }
+//     await ctx.answerCallbackQuery({
+//         text: "You were curious, indeed!",
+//       });
+// })
 
-if (mode === "polling")
-    {bot.start({
-        onStart: ({ username }: { username: string }) =>
-            console.log({
-                msg: "bot running...",
-                username,
-            }),
-    });
-    botcontext = bot;
-}
-else if (mode === "webhook") webhookCallback(bot, "https");
+// // Handle other messages.
+// bot.on("message", async (ctx: any) => {
+//     const messageText = ctx.message.text || "";
+//     console.log("message ==> ", messageText);
+
+//     if (messageText.toLowerCase().startsWith("createtask")) {
+//         const parts = messageText.split(" ");
+
+//         if (parts.length > 1) {
+//             const loading = await ctx.reply("processing...");
+//             const secondWord = parts[1].trim();
+//             const user = nameArray.find((user) => user.name.toLowerCase() === secondWord.toLowerCase());
+
+//             if (!user) {
+//                 const replyText = `📢 Can't find specific name - ${secondWord}`;
+//                 await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
+//                 return  ctx.reply(replyText, { parse_mode: "Markdown" });
+//             }
+
+//             const setting = await getSetting();
+
+//             const thirdWord = parts[2].trim();
+
+//             const sections = await getSections(setting.project);
+
+//             // let section = {};
+//             // let i = 0;
+//             // while(1)
+//             // {
+//             //     let _sectionName = parts.slice(2, 3 + i).join(" ");
+//             //     const _section = sections.map((section:any) => section.name.toLowerCase().startsWith(thirdWord.toLowerCase()));
+//             //     if(_section.length === 1)
+//             //         {
+//             //             section = {..._section}
+//             //             break;
+//             //         }
+//             //     i++;
+//             // }
+
+//             const section = sections.find((section:any) => section.name.toLowerCase().startsWith(thirdWord.toLowerCase()));
+
+//             if (!section) {
+//                 console.log(`Can't find secction from ${messageText}`);
+//                 const replyText = `📢 Can't find secction from ${messageText}`;
+//                 await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
+//                 return  ctx.reply(replyText, { parse_mode: "Markdown" });
+//             }
+
+//             // Join the remaining parts of the message
+//             const command = parts.slice(2+section.name.split(' ').length).join(" ");
+
+//             let body = {
+//                 data: {
+//                     name: command,
+//                     assignee: user.gid,
+//                     resource_subtype: "default_task",
+//                     approval_status: "pending",
+//                     assignee_status: "upcoming",
+//                     assignee_section: section.gid,
+//                     projects: [setting.project],
+//                 },
+//             };
+
+//             const task = await createTask(body);
+
+
+//             const taskName = task.name;
+//             const projectName = task.projects[0].name;
+//             const sectionName = section.name;
+//             const workspaceName = task.workspace.name;
+//             const taskUrl = task.permalink_url;
+//             const replyText =
+//                 `📢 Successfully created a task\n\n` +
+//                 `**${taskName}**\n\n` +
+//                 `🏠 Workspace: **${workspaceName}**\n` +
+//                 `🧰 Project: **${projectName}**\n` +
+//                 `🔖 Section: **${sectionName}**\n\n` +
+//                 `${taskUrl}`;
+//             const replyMarkup = {
+//                 inline_keyboard: [[{ text: "View Task", url: taskUrl }]],
+//             };
+//             await ctx.api.deleteMessage(loading.chat.id, loading.message_id);
+//             await ctx.reply(replyText, { reply_markup: replyMarkup, parse_mode: "Markdown" });
+            
+//         }
+//     }
+// });
+
+// if (mode === "polling")
+//     {bot.start({
+//         onStart: ({ username }: { username: string }) =>
+//             console.log({
+//                 msg: "bot running...",
+//                 username,
+//             }),
+//     });
+//     botcontext = bot;
+// }
+// else if (mode === "webhook") botcontext = webhookCallback(bot, "https");
+
+export default webhookCallback(bot, "https");
